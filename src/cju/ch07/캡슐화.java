aec43af2d9b4a3a683fb079ec21612154ab536cb
qwa1822 <asdzxc1822@naver.com>
2022-07-07 16:11:32 +0900

    캡슐화

--- src/cju/ch07/캡슐화.java
+++ src/cju/ch07/캡슐화.java
@@ -1,8 +1,47 @@
 package cju.ch07;
 
+import java.util.Scanner;
+
+
+class Time{
+   private int hour;
+   private int minute;
+   private int second;
+
+
+
+    public int getHour()
+    {
+        return hour;
+    }
+
+
+   public void setHour(int hour){
+
+        if(isValidHour(hour)) return;
+       this.hour=hour;
+
+   }
+   
+   
+    //매개변수로 넘겨진 hour가 유효한지 확인해서 알려주는메서드
+   private boolean isValidHour(int hour){
+        return hour<0 || hour>23;
+   }
+
+
+
+}
+
 public class 캡슐화 {
     public static void main(String[] args) {
+
+    Time t=new Time();
+    t.setHour(21);
+        System.out.println(t.getHour());
+        t.setHour(100);
 
 
     }
 }
+
