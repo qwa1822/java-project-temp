e350de5cf8a793a42f7717d2e5cdeb60701266a4
qwa1822 <asdzxc1822@naver.com>
2022-07-07 14:26:14 +0900

    오버라이딩

--- src/cju/ch07/오버라이딩.java
+++ src/cju/ch07/오버라이딩.java
@@ -1,7 +1,22 @@
 package cju.ch07;
 
+import java.util.Scanner;
+
+
+class Point{
+    int x;
+    int y;
+
+
+    String getLocation(){
+        return "x:"+x+",y:"+y;
+    }
+}
+
+
 public class 오버라이딩 {
     public static void main(String[] args) {
 
+        Scanner sc=new Scanner(System.in);
     }
 }
