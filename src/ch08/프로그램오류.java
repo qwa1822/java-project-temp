aaee16f2a7de4e4812defe91f2fdd261553d0bf2
qwa1822 <asdzxc1822@naver.com>
2022-08-01 13:58:07 +0900

    내부클래스

--- src/ch08/프로그램오류.java
+++ src/ch08/프로그램오류.java
@@ -3,21 +3,18 @@ package ch08;
 public class 프로그램오류 {
     public static void main(String[] args) {
 
-        System.out.println(1);
-        System.out.println(2);
 
-        try{
-            System.out.println(3);
-            System.out.println(0/0);
-            System.out.println(4);
-        }catch(ArithmeticException e){
-            System.out.println("true");
-            System.out.println("ArithemticException");
-        } catch(Exception e){
-            System.out.println("Exception");
-            
-            //모든 예외처리
+
+
+        int num=12345;
+        int sum=0;
+
+        while(num>0){
+
+            sum+=num%10;
+            num/=10;
+
         }
-        System.out.println(6);
+        System.out.println(sum);
     }
 }
